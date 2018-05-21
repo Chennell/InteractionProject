9cd0104bbff55009cad7d0a545ff22ac59ed69c8
Chennell <jessica_chennell@hotmail.com>
2018-05-21 11:35:23 +0200

    Should work?

---
+++ Interaction project/js/main.js
@@ -0,0 +1,29 @@
+/*****This is the menu******/
+//Here we set the variable Opened to flase
+var opened = false;
+
+//Waits for the document to be ready
+$(document).ready(function(){
+    
+    
+    //This is the function that open and closes the menu
+	$('.hamburger').click(function(){
+        
+        //If the opened is true and you click the cross, the dropdown should slide up and in the less file the hamburger changes from cross to hamburger, we set a background color and opened to false
+        //The else statment tells us the oposite
+        if(opened){
+            $("nav").addClass("closing");
+            $("nav").removeClass("slidedown");
+//            $(".line").removeClass("cross");
+            opened = false;
+            
+        } else {
+            $("nav").addClass("slidedown");
+            $("nav").removeClass("closing");
+            opened = true;
+            
+        }
+        //Here we add a toggleClass to all the children in hamburger, wich is all the lines, so that it will responed to the styling making it to a cross
+		$('.hamburger').children().toggleClass('open');
+	});
+});
