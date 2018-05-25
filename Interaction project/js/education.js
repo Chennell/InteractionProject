59e38848bedf4af7292117467762e976eb2bb728
juliapalm <32334112+juliapalm@users.noreply.github.com>
2018-05-25 13:08:02 +0200

    Education

--- Interaction project/js/education.js
+++ Interaction project/js/education.js
@@ -1,3 +1,35 @@
 $(document).ready(function() {
 
+  //NOTIFICATION
+  $(".notification-box").addClass("slide-up");
+
+  $(".yes-btn").click( function(event) {
+    event.preventDefault();
+    $(".notification-box").addClass("slide-left");
+    setTimeout(xdown, 10000);
+  });
+
+  $(".no-btn").click( function(event) {
+    event.preventDefault();
+    $(".notification-box").addClass("slide-down");
+  });
+
+  $(".x").click( function(event) {
+    event.preventDefault();
+    $(".notification-box").addClass("slide-down-x");
+  });
+
+  //COOKIE
+  $(".cookie-box").addClass("slide-up");
+
+  $(".cookie-ok").click( function(event) {
+    event.preventDefault();
+    $(".cookie-box").addClass("slide-down");
+  });
+
 });
+
+//SET TIMEOUT NOTIFICATION
+function xdown() {
+  $(".notification-box").addClass("slide-down-x");
+}
